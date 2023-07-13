89884108da54531eb833df1f58e4f8479f6fc2f3
sanko <sang.koh@hotmail.com>
2023-07-13 21:13:13 +1000

    event type test

--- index.js
+++ index.js
@@ -7,19 +7,43 @@ const port = process.env.PORT || 3000;
 const path = require("path");
 app.use(express.static(path.join(__dirname, "static")));
 
+let current = 0;
+const clients = new Map();
+
+function send(client, event, data) {
+	if (!clients.has(client)) return;
+
+	const info = clients.get(client);
+
+	client.write(`event: ${event}\n`);
+	client.write(`data: ${JSON.stringify(data)}\n\n`);
+}
+
 app.get("/event", (req, res) => {
+	const id = current++;
+
+	console.log(id, "connect");
+	clients.set(res, {id});
+
 	res.writeHead(200, {
 		"Content-Type": "text/event-stream",
 		"Cache-Control": "no-cache",
 		"Connection": "keep-alive",
 	});
 
-	setInterval(() => {
-		res.write("data: hello " + String(Math.random()) + "\n\n");
-	}, 500);
-
 	req.on("close", () => {
-		console.log("closed");
+		console.log(id, "close");
+
+		clients.forEach((info, client) => {
+			send(client, "leave", {id});
+		});
+	});
+
+	send(res, "setid", id);
+	clients.forEach((info, client) => {
+		if (client === res) return;
+
+		send(client, "enter", {id});
 	});
 });
 
--- static/script.js
+++ static/script.js
@@ -1,8 +1,10 @@
 const es = new EventSource("event");
 const out = document.getElementById("out");
 
-es.onmessage = event => {
-	const p = document.createElement("p");
-	p.textContent = event.data;
-	out.appendChild(p);
-};
+["message", "setid", "enter", "leave"].forEach(type => {
+	es.addEventListener(type, event => {
+		const p = document.createElement("p");
+		p.textContent = `${type}: ${event.data}`;
+		out.appendChild(p);
+	});
+});
